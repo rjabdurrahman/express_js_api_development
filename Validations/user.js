edda488b6b005c17b10c7b96ba67ea1f22dac29e
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-12-01 23:43:15 +0600

    Added: Valiations in validation folder

---
+++ Validations/user.js
@@ -0,0 +1,24 @@
+const { check } = require('express-validator')
+
+const signupValidations = [
+    check('name')
+        .notEmpty().withMessage('Name is required!')
+        .isLength({ min: 3 }).withMessage('Name should be at least 3 characters!')
+    ,
+    check('email')
+        .notEmpty().withMessage('Email is required!')
+        .isEmail().withMessage('Invalid email format!')
+    ,
+    check('password')
+        .notEmpty().withMessage('Password is required!')
+        .isLength({ min: 6 }).withMessage('Password should be at least 6 characters!')
+        .matches('[a-z]').withMessage('Password should contain at leas one alphabet!')
+    ,
+    check('phone')
+        .notEmpty().withMessage('Phone is required!')
+        .isMobilePhone().withMessage('Invalid Phone Number')
+    ,
+]
+
+
+module.exports = { signupValidations }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { urlencoded } = require('express');
 const express = require('express');
-const { check } = require('express-validator')
 const app = express()
 const connectDB = require('./utils/db')
 const Home = require('./Controllers/Home')
 const ProductCotroller = require('./Controllers/Product')
 const UserController = require('./Controllers/User')
+const { signupValidations } = require('./Validations/user')
 
 connectDB()
 
@@ -13,29 +13,7 @@ app.use(urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', Home.get)
-app.post(
-    '/signup',
-    [
-        check('name')
-        .notEmpty().withMessage('Name is required!')
-        .isLength({ min: 3}).withMessage('Name should be at least 3 characters!')
-        ,
-        check('email')
-        .notEmpty().withMessage('Email is required!')
-        .isEmail().withMessage('Invalid email format!')
-        ,
-        check('password')
-        .notEmpty().withMessage('Password is required!')
-        .isLength({ min: 6}).withMessage('Password should be at least 6 characters!')
-        .matches('[a-z]').withMessage('Password should contain at leas one alphabet!')
-        ,
-        check('phone')
-        .notEmpty().withMessage('Phone is required!')
-        .isMobilePhone().withMessage('Invalid Phone Number')
-        ,
-    ],
-    UserController.signup
-)
+app.post('/signup', signupValidations, UserController.signup)
 app.post('/login', UserController.login)
 
 app.get('/products', ProductCotroller.get)
